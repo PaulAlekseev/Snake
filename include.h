6e89a16a750ff920160561802d2a132a6d98c3d0
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-02-21 00:30:39 +0300

    working on snake class

--- include.h
+++ include.h
@@ -3,3 +3,20 @@
 
 
 using namespace std;
+
+class Snake 
+{
+public:
+    int length;
+    int direction;
+    int posX;
+    int posY;
+    
+    Snake(int length, int direction, int posX, int posY)
+    {
+        this->length = length;
+        this->direction = direction;
+        this->posX = posX;
+        this->posY = posY;
+    }
+};
