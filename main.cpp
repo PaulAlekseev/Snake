a2f815765636567a49f78ec13e62c3142ab6cb08
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-02-23 22:39:38 +0300

    fruit test

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 
 int main(){
 
-    Snake snake(10, 3, 12, 12);
+    Snake snake(9, 3, 12, 12);
+
+    Fruit fruit(30, 30);
 
     WindowArea area(1920, 1080);
 
@@ -13,17 +15,18 @@ int main(){
 
     sf::Clock clock;
 
+    sf::Texture body, fruit1;
+    body.loadFromFile("Textures/body1.png");
+    fruit1.loadFromFile("Textures/fruit.png");
+
+    sf::Sprite sprite(body);
+    sf::Sprite fruitsprite(fruit1);
+
     
     while (window.isOpen())
     {
 
         sf::Time elapsed1 = clock.getElapsedTime();
-
-        sf::Texture body;
-        body.loadFromFile("Textures/body1.png");
-
-        sf::Sprite sprite(body);
-        
 
         sf::Event event;
         while(window.pollEvent(event))
@@ -42,6 +45,11 @@ int main(){
 
 
         window.clear();
+        fruitsprite.setPosition(fruit.posX*area.gridScaleX, fruit.posY*area.gridScaleY);
+        window.draw(fruitsprite);
+
+        sprite.setPosition(snake.headX*area.gridScaleX, snake.headY*area.gridScaleY);
+        window.draw(sprite);
 
         for (int i = 0; i < snake.length; i++)
         {
@@ -52,9 +60,15 @@ int main(){
         window.display();
         if(elapsed1.asSeconds() > 0.1)
         {
+
+            if (fruit.gotEaten(snake.headX, snake.headY))
+            {
+                snake.growth();
+            }
             snake.changedirection(direction);
             snake.step();
             clock.restart();
+            cout << snake.length;
         }
     }
 }
